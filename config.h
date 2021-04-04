b11efc42f71f3ab7ad5899c1c339be59bfad8fe7
Simon Striekwold <info@simonstriekwold.nl>
2021-04-04 14:07:50 +0200

    patched in emoji support

--- config.h
+++ config.h
@@ -6,9 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "DinaRemaster:pixelsize=16:antialias=false:autohint=false";
-static char *font2[] = {
-    "EmojiOne:pixelsize=16:antialias=true:autohint=true",
-};
+static char *font2[] = {"JoyPixels:pixelsize=10:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 /*
