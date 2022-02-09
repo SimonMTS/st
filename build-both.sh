6c9a9ef2d593edd3dd22c414196d7a971bc1cd6b
Simon Striekwold <info@simonstriekwold.nl>
2022-02-09 20:38:04 +0100

    added "installer" & dot-desktop files

--- build-both.sh
+++ build-both.sh
@@ -7,3 +7,8 @@ rm ./st/config.h
 (cd st && cat font2.h color.h config.base.h > config.h)
 (cd st && sudo make clean st)
 mv -f ./st/st ./st-fira
+
+cp -f ./st-dina ~/.scripts/st
+cp -f ./st-fira ~/.scripts/st-fira
+cp -f ./st-fira.desktop ~/.local/share/applications/st-fira.desktop
+cp -f ./st.desktop ~/.local/share/applications/st.desktop
