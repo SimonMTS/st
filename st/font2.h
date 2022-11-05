9b420b7c3b959949364f21def66c7fd5b9eeb218
Simon Striekwold <info@simonstriekwold.nl>
2022-11-05 16:29:16 +0100

    reduced fira font size

--- st/font2.h
+++ st/font2.h
@@ -1,3 +1,3 @@
-static char *font = "FiraCode-Bold:pixelsize=22:antialias=true:autohint=false";
-static char *font2[] = {"JoyPixels:pixelsize=16:antialias=true:autohint=true"};
+static char *font = "FiraCode-Bold:pixelsize=14:antialias=true:autohint=false";
+static char *font2[] = {"JoyPixels:pixelsize=10:antialias=true:autohint=true"};
 // libxft-bgra // https://wiki.archlinux.org/title/St#Crashes_if_page_contains_emoji_characters
